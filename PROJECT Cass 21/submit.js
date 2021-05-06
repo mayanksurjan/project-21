5a40dd95c2f4b6cfcd97d56743d0bc42ba212192
mayanksurjan <81294824+mayanksurjan@users.noreply.github.com>
2021-05-06 21:42:58 +0530

    Add files via upload

---
+++ PROJECT Cass 21/submit.js
@@ -0,0 +1,60 @@
+var canvas;
+var block1,block2,block3,block4;
+var ball, edges;
+var music;
+
+function preload(){
+    // load sound here
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    block1 = createSprite(0,580,360,30);
+    block1.shapeColor = "blue";
+
+    block2 = createSprite(295,580,200,30);
+    block2.shapeColor = "orange";
+
+    block3 = createSprite(495,580,200,30);
+    block3.shapeColor = "green";
+
+    block4 = createSprite(695,580,200,30);
+    block4.shapeColor = "red";
+
+    //create two more blocks i.e. block3 and block4 here
+
+    ball = createSprite(random(20,750),100, 40,40);
+    ball.shapeColor = rgb(255,255,255);
+    //write code to add velocityX and velocityY
+ball.velocityY=-4
+ball.velocityX=4
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    edges=createEdgeSprites();
+    ball.bounceOff(edges);
+
+    
+    //write code to bounce off ball from the block1 
+    if(block1.isTouching(ball) ){
+        ball.shapeColor = block1.shapeColor;
+        music.play();
+    }
+ball.bounceOff(block1)
+
+
+
+
+
+   
+
+    //write code to bounce off ball from the block3
+
+
+    //write code to bounce off ball from the block4
+
+    drawSprites();
+}
